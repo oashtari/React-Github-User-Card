adde63acb4d51e5ce0782a2c7b01edc9d8141351
oashtari <oashtari@gmail.com>
2020-01-17 21:12:29 -0800

    added styling to cards

--- github_user_card/src/Components/FollowerCard.js
+++ github_user_card/src/Components/FollowerCard.js
@@ -6,20 +6,18 @@ const FollowerCard = props => {
 
 
     return (
-        <>
-            {/* {console.log('TSTING', props.followers)} */}
-            {/* {props.followers &&
-                props.followers.map(follower =>  */}
-            <div className='followers'>
-                <h4 className='name'>Name: {props.follower.name}</h4>
-                <h5 className='location'>Location: {props.follower.location}</h5>
-                <p className='bio'>Bio: {props.follower.bio}</p>
-                <h5 className='repo'>Public Repo: {props.follower.public_repo}</h5>
-                {/* <h6 className='followers'>Followers: {props.followers.followers}</h6>
-                <h6 className='following'>Following: {props.followers.following}</h6> */}
-            </div>
-        </>
-        // ))
+        <div style={{ margin: '20px' }}>
+            <Card style={{ backgroundColor: 'blue', width: '500px' }} className='allFollowers'>
+                <CardTitle style={{ backgroundColor: 'pink' }} className='name'>Name: {props.follower.name}</CardTitle>
+                <Card style={{ backgroundColor: 'red' }}>
+                    <h5 className='location'>Location: {props.follower.location}</h5>
+                    <Card style={{ backgroundColor: 'orange' }} className='bio'>Bio: {props.follower.bio}</Card>
+                    <h5 className='repo'>Public Repo: {props.follower.public_repos}</h5>
+                    <h6 className='followers'>Followers: {props.follower.followers}</h6>
+                    <h6 className='following'>Following: {props.follower.following}</h6>
+                </Card>
+            </Card>
+        </div>
     )
 }
 
--- github_user_card/src/Components/UserCard.js
+++ github_user_card/src/Components/UserCard.js
@@ -4,18 +4,19 @@ import '../App.css';
 
 const UserCard = props => {
     return (
-
-        <Card style={{ backgroundColor: 'blue', width: '500px' }} className='user'>
-            {/* {console.log('USER TESTING', props)} */}
-            <CardTitle style={{ backgroundColor: 'pink' }} className='name'>Name: {props.user.name}</CardTitle>
-            <Card style={{ backgroundColor: 'red' }}>
-                <CardText className='location'>Location: {props.user.location}</CardText>
-                <Card style={{ backgroundColor: 'orange' }} className='bio'>Bio: {props.user.bio}</Card>
-                <CardText className='repo'>Public Repo: {props.user.public_repo}</CardText>
-                <CardText className='followers'>Followers: {props.user.followers}</CardText>
-                <CardText className='following'>Following: {props.user.following}</CardText>
+        <div style={{ margin: '20px' }}>
+            <Card style={{ backgroundColor: 'blue', width: '500px' }} className='user'>
+                {/* {console.log('USER TESTING', props)} */}
+                <CardTitle style={{ backgroundColor: 'pink' }} className='name'>Name: {props.user.name}</CardTitle>
+                <Card style={{ backgroundColor: 'red' }}>
+                    <CardText className='location'>Location: {props.user.location}</CardText>
+                    <Card style={{ backgroundColor: 'orange' }} className='bio'>Bio: {props.user.bio}</Card>
+                    <CardText className='repo'>Public Repo: {props.user.public_repos}</CardText>
+                    <CardText className='followers'>Followers: {props.user.followers}</CardText>
+                    <CardText className='following'>Following: {props.user.following}</CardText>
+                </Card>
             </Card>
-        </Card>
+        </div>
     )
 }
 
